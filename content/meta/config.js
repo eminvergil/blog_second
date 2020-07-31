27a4f8e0ee0274f7c583f446635b48ed55da2de9
eminvergil <emintrhack@gmail.com>
2020-07-31 10:44:13 +0300

    init

---
+++ content/meta/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: "Emin Vergil", // <title>
+  shortSiteTitle: "Emin Vergil", // <title> ending for posts and pages
+  siteDescription: "Emin Vergil",
+  siteUrl: "https://www.eminvergil.github.io/blog",
+  // pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+
+  /* author */
+  authorName: "Emin Vergil",
+  authorTwitterAccount: "https://twitter.com/eminvergil",
+  authorGithub: "https://github.com/eminvergil",
+  authorStackoverflow: "https://github.com/eminvergil",
+  authorLinkedin: "https://github.com/eminvergil",
+  authorCodeforces: "https://github.com/eminvergil",
+  authorYoutube: "https://github.com/eminvergil",
+
+  /* header text */
+  headerTitle: "emin vergil",
+  headerSubTitle: "",
+
+  /* manifest.json */
+  manifestName: "Emin Vergil",
+  manifestShortName: "eminVergil", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: ""
+};
---
+++ content/pages/1--about/index.md
@@ -0,0 +1,10 @@
+---
+title: About
+---
+
+<re-img src="large.jpeg" hovereffect=true></re-img>
+
+Hi there. I'm Emin Vergil and this is my personal blog.
+I ramble about code, data, and other geeky interests.
+
+<re-icons></re-icons>
