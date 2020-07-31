ccd603d627afbf7bb27f3c8347b693eb9a0cc1f9
eminvergil <emintrhack@gmail.com>
2020-07-31 11:11:52 +0300

    gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ const queries = [
 
 module.exports = {
   // pathPrefix: config.pathPrefix,
-
+  pathPrefix: "/blog",
   // If you want a Search page, put this back in plugins:
   /*
     {
@@ -70,7 +70,7 @@ module.exports = {
         isTSX: true, // defaults to false
         //jsxPragma: `jsx`, // defaults to "React" ??
         allExtensions: true
-      },
+      }
     },
     {
       resolve: `gatsby-plugin-layout`,
@@ -88,7 +88,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/${process.env.POSTS_FOLDER || 'mock_posts'}/`,
+        path: `${__dirname}/content/${process.env.POSTS_FOLDER || "mock_posts"}/`,
         name: "posts"
       }
     },
@@ -120,18 +120,18 @@ module.exports = {
             options: {
               maxWidth: 800,
               backgroundColor: "transparent",
-              tracedSVG: { color: '#f9ebd2' }
+              tracedSVG: { color: "#f9ebd2" }
             }
           },
           {
             resolve: `gatsby-remark-rehype-images`,
             options: {
-              tag: 're-img',
+              tag: "re-img",
               maxWidth: 800,
               quality: 90,
               webP: true,
-              toFormat: 'WEBP',
-              tracedSVG: { color: '#f9ebd2' },
+              toFormat: "WEBP",
+              tracedSVG: { color: "#f9ebd2" },
               generateTracedSVG: true
             }
           },
